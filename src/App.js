5742b473f23810de2dafb25534ac3c8867494071
hhrum <denis.smit72@gmail.com>
2021-04-11 00:31:13 +0500

    Fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenS
 import { AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
+import Home from './panels/Home';
 import Register from './panels/Register';
 
 const App = () => {
